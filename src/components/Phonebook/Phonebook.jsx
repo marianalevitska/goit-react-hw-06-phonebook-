57fc6254d88f568ff26fb62b1671dd25b465643f
Mariana <mariamna5204@gmail.com>
2022-08-30 00:03:08 +0300

    add redux toolkit

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,6 +1,5 @@
-import { useState, useCallback, useEffect, useRef } from 'react';
-
-import { nanoid } from 'nanoid';
+import { useState, useCallback } from 'react';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
 
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
@@ -8,65 +7,36 @@ import Filter from './Filter';
 
 import stl from './phonebook.module.css';
 
+import { getContacts } from '../../redux/contacts/contacts-selectors';
+import { actions } from 'redux/contacts/contacts-slice';
+
 function Phonebook() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const contacts = useSelector(getContacts, shallowEqual);
+
+  const dispatch = useDispatch();
 
   const [filter, setFilter] = useState('');
   // get files from local storage
 
-  const firstRender = useRef(true);
-
-  useEffect(() => {
-    if (firstRender.current) {
-      firstRender.current = false;
-      const data = localStorage.getItem('contacts');
-      const contacts = JSON.parse(data); //todo JSON.parse - convert string to object
-      if (contacts?.length) {
-        setContacts(contacts);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!firstRender.current) {
-      localStorage.setItem('contacts', JSON.stringify(contacts)); //todo JSON.parse - convert string to object
-    }
-  }, [contacts]);
-
   const addContacts = useCallback(
-    ({ name, number }) => {
-      const newContactName = name.toLowerCase();
+    data => {
+      const newContactName = data.name.toLowerCase();
       const isDublicate = contacts.find(
         item => item.name.toLowerCase() === newContactName
       );
       if (isDublicate) {
-        alert(`${name} is alredy in your phonebook`);
+        alert(`${data.name} is already in your phonebook`);
         return;
       }
 
-      setContacts(prevContacts => {
-        const newContact = {
-          id: nanoid(),
-          name,
-          number,
-        };
-        return [...prevContacts, newContact];
-      });
+      return dispatch(actions.addContacts(data));
     },
-    [contacts, setContacts]
+    [contacts, dispatch]
   );
 
   const deleteContact = useCallback(
-    id => {
-      const newContacts = contacts.filter(item => item.id !== id);
-      setContacts(newContacts);
-    },
-    [contacts, setContacts]
+    id => dispatch(actions.deleteContacts(id)),
+    [dispatch]
   );
 
   const filterChange = useCallback(
